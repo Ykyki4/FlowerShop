cd875c6d5c8e007ba06b47b9754def623fc350df
0viktory0 <36894469+0viktory0@users.noreply.github.com>
2023-01-19 18:21:04 +0300

    completed admin

--- backend/admin.py
+++ backend/admin.py
@@ -1,20 +1,29 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Bouquet, Order, Consultation
 
 
 @admin.register(Bouquet)
 class BouquetAdmin(admin.ModelAdmin):
-    list_display = ('title', 'price', 'reason', 'description')
+
+    list_display = ('title', 'price', 'reason', 'description', 'image_tag',)
+    list_filter = ('reason',)
+    readonly_fields = ['image_tag',]
+
+    def image_tag(self, obj):
+        return format_html('<img src="{}" style="max-height: 100px;"/>'.format(obj.image.url))
+
+    image_tag.short_description = 'Картинка'
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     raw_id_fields = ('bouquet',)
-    list_display = ('bouquet', 'client_name', 'phonenumber', 'address', 'delivery_time', 'is_payed')
-
+    list_display = ('bouquet', 'client_name', 'phonenumber', 'address', 'delivery_time', 'is_payed', 'is_delivered',)
+    list_filter = ('bouquet', 'is_payed', 'is_delivered',)
 
 @admin.register(Consultation)
 class ConsultationAdmin(admin.ModelAdmin):
-    list_display = ('client_name', 'phonenumber', 'created_at', 'is_closed', 'comment')
-    list_editable = ('is_closed', 'comment',)
+    list_display = ('client_name', 'phonenumber', 'created_at', 'is_closed', 'comment',)
+
     
